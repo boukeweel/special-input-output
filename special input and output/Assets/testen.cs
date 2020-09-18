f7d4bc66f92d48464548df8c99a5c1120fb2df7a
Bouke <BoukeWeel@outlook.com>
2020-09-18 14:17:18 +0200

    tele werkt

--- special input and output/Assets/testen.cs
+++ special input and output/Assets/testen.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class testen : MonoBehaviour
 {
+    public float speed;
+
+    bool holdbutton = false;
+
+    Rigidbody rig;
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>()
+    }
+    private void Update()
+    {
+        if(holdbutton)
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
     public void test()
     {
+        holdbutton = true;
         Debug.Log("werkt?");
     }
 }
