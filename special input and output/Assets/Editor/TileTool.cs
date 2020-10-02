e0761c8a9dab9876559e495a3d446c793738d338
Bouke <BoukeWeel@outlook.com>
2020-10-02 13:38:44 +0200

    start with map

---
+++ special input and output/Assets/Editor/TileTool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class TileTool : EditorWindow
+{
+    [MenuItem("Tools/tile tool")]
+    public static void showWindow()
+    {
+        EditorWindow.GetWindow<TileTool>("tile tool");
+    }
+    private void OnGUI()
+    {
+        if (GUILayout.Button("collider and layer"))
+        {
+            AddLayerMask();
+            AddMeshCollider();
+            //Make_Prefab();
+        }
+        if (GUILayout.Button("prefab"))
+        {
+            Make_Prefab();
+        }
+    }
+
+    private void AddLayerMask()
+    {
+        foreach(GameObject obj in Selection.gameObjects)
+        {
+            obj.layer = LayerMask.NameToLayer("Ground");
+        }
+    }
+    private void AddMeshCollider()
+    {
+       
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            obj.AddComponent<MeshCollider>();
+        }
+    }
+    private void Make_Prefab()
+    {
+
+        //make a prefab from all slected objects and put it in the prefabes folder
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            string localpath = "Assets/prefabs/test/" + obj.name + ".prefab";
+
+            localpath = AssetDatabase.GenerateUniqueAssetPath(localpath);
+
+            PrefabUtility.SaveAsPrefabAssetAndConnect(obj, localpath, InteractionMode.UserAction);
+        }
+    }
+
+}
---
+++ special input and output/Assets/scripts/Car_controller.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_controller : MonoBehaviour
+{
+
+    public Rigidbody sph_Rig;
+
+    public float forwardSpeed = 8f;
+    public float backwardSpeed = 4f;
+    public float maxSpeed = 50f;
+    public float turnStrength = 180f;
+    
+
+    private float speedInput;
+    private float turnInput;
+
+
+    public float gravaityForce = 10f;
+    public bool grounded;
+
+    public LayerMask whatIsGround;
+    public float groundRayLength = 0.5f;
+
+    public Transform groundRayPoint;
+
+
+    public float r_drag = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sph_Rig.transform.parent = null;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedInput = 0f;
+        //turn
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardSpeed * 1000f;
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * backwardSpeed * 1000f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+        
+
+        transform.position = sph_Rig.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit;
+        //check if the car is on the ground
+
+        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+
+        if (grounded)
+        {
+            sph_Rig.drag = r_drag;
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                sph_Rig.AddForce(transform.forward * speedInput);
+            }
+        }
+        else
+        {
+            sph_Rig.drag = 0.1f;
+            sph_Rig.AddForce(Vector3.up * -gravaityForce * 100f);
+        }
+    }
+}
