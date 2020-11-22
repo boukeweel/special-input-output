5b21df8925c5723b3764774823d88739609c7447
Bouke <BoukeWeel@outlook.com>
2020-11-22 14:10:39 +0100

    objectives

--- special input and output/Assets/scripts/Misions/MisionLocations.cs
+++ special input and output/Assets/scripts/Misions/MisionLocations.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Mission Locations", menuName = "Location")]
 public class MisionLocations : ScriptableObject
 {
-    public Transform location;
-    public GameObject partecals;
+    [Header("voor de normaal Ground is it 0.6 for 1 up is .............")]
+    public Vector3 location = new Vector3(0, 0.6f, 0);
+    public GameObject spawnobj;
     public float time;
-
+    public int coins;
 }
